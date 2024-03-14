eac6867039c4e79cd008e00a541615304cfaa6c9
Federico.Murru <djfedemurru@icloud.com>
2024-03-14 09:10:49 +0000

    1

--- Lezione/blog-app/resources/views/welcome.blade.php
+++ Lezione/blog-app/resources/views/welcome.blade.php
@@ -8,13 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
     <title>Document</title>
+    <nav class="nav">
+        <a class="nav-link" href="{{ route('welcome') }}">Home</a>
+        <a class="nav-link" href="{{ route('articles') }}">Articles</a>
+        <a class="nav-link" href="{{ route('about_me') }}">Chi Sono</a>
+        <a class="nav-link" href="{{ route('contacts') }}">Contatti</a>
+    </nav>
 </head>
 <body>
     <h1>Benvenuto</h1>
 
-    <a href="/contatti">contatti</a>
-    <a href="/chi_sono">chi sono</a>
-
+   
 
 </body>
 </html>
