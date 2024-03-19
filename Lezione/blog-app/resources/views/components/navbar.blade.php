02ab358cb675cafd6604feef5be8c79b9f8f87fa
Federico.Murru <djfedemurru@icloud.com>
2024-03-19 13:49:17 +0000

    blog

--- Lezione/blog-app/resources/views/components/navbar.blade.php
+++ Lezione/blog-app/resources/views/components/navbar.blade.php
@@ -1,6 +1,8 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 <nav class="navbar navbar-expand-sm bg-body-warning">
     <div class="container-fluid">
-        <a class="nav-link active py-2 px-3" aria-current="page" href="{{ route('welcome') }}">Home</a>
+        <a class="nav-link active py-2 px-3" aria-current="page" href="{{ route('welcome') }}">{{config('app.name')}}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -10,6 +12,24 @@
                 <a class="nav-link py-2 px-3" href="{{ route('about') }}">Chi Sono</a>
                 <a class="nav-link py-2 px-3" aria-current="page" href="{{ route('contacts') }}">Contatti</a>
             </div>
-        </div>
     </div>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle nav-link py-2 px-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+            </ul>
+          </li>
+    </ul>
+    </div>
+</div>
 </nav>
+
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
