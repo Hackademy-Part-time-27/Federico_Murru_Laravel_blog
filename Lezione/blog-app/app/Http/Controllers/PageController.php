3882514a60a1805fa545f419162e8d1853734d74
Federico.Murru <djfedemurru@icloud.com>
2024-03-19 08:36:46 +0000

    redundant code

--- Lezione/blog-app/app/Http/Controllers/PageController.php
+++ Lezione/blog-app/app/Http/Controllers/PageController.php
@@ -7,13 +7,24 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
 
+    public $articles;
+    public function __construct()
+    {
+        $this->articles = [
+            ['title' => 'Perché JS è migliore di PHP', 'category' => 'Categoria: Programmazione JS', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+            ['title' => 'Perché PHP è migliore di JS', 'category' => 'Categoria: Programmazione PHP', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+            ['title' => 'A me non piace nessuno dei due', 'category' => 'Categoria: Programmazione generica', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        ];
+    }
+
+
     public function welcome()
     {
         return view('welcome', [
             "title" => "Ciao! Questo e il mio blog",
             "description" => "Benvenuti nel mio blog Laravel! Qui 
-            troverete una raccolta delle mie esperienze,
-            conoscenze e risorse nel mondo dello sviluppo web con Laravel."
+        troverete una raccolta delle mie esperienze,
+        conoscenze e risorse nel mondo dello sviluppo web con Laravel."
         ]);
     }
     public function contacts()
@@ -30,28 +41,14 @@ class PageController extends Controller
     }
     public function articles()
     {
-        $articles = [
-            ['title' => 'Perché JS è migliore di PHP', 'description' => '...'],
-            ['title' => 'Perché PHP è migliore di JS', 'description' => '...'],
-            ['title' => 'A me non piace nessuno dei due', 'description' => '...'],
-        ];
-
         return view(
             'pages.articles',
-            ['articles' => $articles],
+            ['articles' => $this->articles],
             ["title" => "Articoli",]
         );
     }
     public function article($id)
     {
-        $index = $id;
-
-        $articles = [
-            ['title' => 'Perché JS è migliore di PHP', 'category' => 'Categoria: Programmazione JS', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-            ['title' => 'Perché PHP è migliore di JS', 'category' => 'Categoria: Programmazione PHP', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-            ['title' => 'A me non piace nessuno dei due', 'category' => 'Categoria: Programmazione generica', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-        ];
-
-        return view('pages.article', ['article' => $articles[$index]]);
+        return view('pages.article', ['article' => $this->articles[$id]]);
     }
 }
