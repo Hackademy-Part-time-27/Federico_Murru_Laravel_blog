1724ff011815465993a552b978194e040ba9de1b
Federico.Murru <djfedemurru@icloud.com>
2024-03-19 08:26:01 +0000

    controller2

--- Lezione/blog-app/app/Http/Controllers/PageController.php
+++ Lezione/blog-app/app/Http/Controllers/PageController.php
@@ -6,8 +6,52 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+
+    public function welcome()
+    {
+        return view('welcome', [
+            "title" => "Ciao! Questo e il mio blog",
+            "description" => "Benvenuti nel mio blog Laravel! Qui 
+            troverete una raccolta delle mie esperienze,
+            conoscenze e risorse nel mondo dello sviluppo web con Laravel."
+        ]);
+    }
     public function contacts()
     {
         return view('pages.contacts');
+    }
+
+    public function aboutUs()
+    {
+        return view('pages.about-me', [
+            "title" => "Titolo dinamico",
+            "description" => "Breve descrizione ma dinamica"
+        ]);
+    }
+    public function articles()
+    {
+        $articles = [
+            ['title' => 'Perché JS è migliore di PHP', 'description' => '...'],
+            ['title' => 'Perché PHP è migliore di JS', 'description' => '...'],
+            ['title' => 'A me non piace nessuno dei due', 'description' => '...'],
+        ];
+
+        return view(
+            'pages.articles',
+            ['articles' => $articles],
+            ["title" => "Articoli",]
+        );
+    }
+    public function article($id)
+    {
+        $index = $id;
+
+        $articles = [
+            ['title' => 'Perché JS è migliore di PHP', 'category' => 'Categoria: Programmazione JS', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+            ['title' => 'Perché PHP è migliore di JS', 'category' => 'Categoria: Programmazione PHP', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+            ['title' => 'A me non piace nessuno dei due', 'category' => 'Categoria: Programmazione generica', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        ];
+
+        return view('pages.article', ['article' => $articles[$index]]);
     }
 }
--- Lezione/blog-app/routes/web.php
+++ Lezione/blog-app/routes/web.php
@@ -4,64 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
 
-Route::get('/', function () {
-    return view('welcome', [
-        "title" => "Ciao! Questo e il mio blog",
-        "description" => "Benvenuti nel mio blog Laravel! Qui 
-        troverete una raccolta delle mie esperienze,
-         conoscenze e risorse nel mondo dello sviluppo web con Laravel."
-    ]);
-})->name('welcome');
-
-
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
 
 Route::get('/contacts-us', [PageController::class, 'contacts'])->name('contacts');
 
+Route::get('/about-me', [PageController::class, 'aboutUs'])->name('about');
 
-Route::get('/about-me', function () {
-    return view('pages.about-me', [
-        "title" => "Titolo dinamico",
-        "description" => "Breve descrizione ma dinamica"
-    ]);
-})->name('about');
+Route::get('/articles', [PageController::class, 'articles'])->name('articles');
 
-// Route::get('/{id}', function ($id) {
-//     return "This is " . $id;
-// });
-
-
-
-Route::get('/articles', function () {
-
-    $articles = [
-        ['title' => 'Perché JS è migliore di PHP', 'description' => '...'],
-        ['title' => 'Perché PHP è migliore di JS', 'description' => '...'],
-        ['title' => 'A me non piace nessuno dei due', 'description' => '...'],
-    ];
-
-    //$articles = [];
-
-    return view(
-        'pages.articles',
-        ['articles' => $articles],
-        ["title" => "Articoli",]
-    );
-})->name('articles');
-
-
-Route::get('/article/{id?}', function ($id) {
-
-    $index = $id;
-
-    $articles = [
-        ['title' => 'Perché JS è migliore di PHP', 'category' => 'Categoria: Programmazione JS', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-        ['title' => 'Perché PHP è migliore di JS', 'category' => 'Categoria: Programmazione PHP', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-        ['title' => 'A me non piace nessuno dei due', 'category' => 'Categoria: Programmazione generica', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
-    ];
-
-    if (array_key_exists($index, $articles)) {
-        return view('pages.article', ['article' => $articles[$index]]);
-    } else {
-        abort(404);
-    }
-})->name('article');
+Route::get('/article/{id?}', [PageController::class, 'article'])->name('article');
