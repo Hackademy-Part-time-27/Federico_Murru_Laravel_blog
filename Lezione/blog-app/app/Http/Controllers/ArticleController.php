465ffaa1686789f45fdc3fcb2184d01e2bee88e1
Federico.Murru <djfedemurru@icloud.com>
2024-03-28 12:38:57 +0000

    article

--- Lezione/blog-app/app/Http/Controllers/ArticleController.php
+++ Lezione/blog-app/app/Http/Controllers/ArticleController.php
@@ -3,31 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Article;
+
 
 class ArticleController extends Controller
 {
     public function create()
     {
+        ///1° metodo
+        // $article = new Article();
 
-        return "Sono in create";
-        /* 1° metodo
-        $article = new Article();
+        // $article->title = 'Perché JS è migliore di PHP';
+        // $article->category = 'Dev';
+        // $article->description = '...';
 
-        $article->title = 'Perché JS è migliore di PHP';
-        $article->category = 'Dev';
-        $article->description = '...';
-
-        $article->save();
-        */
+        // $article->save();
 
         /* 2° metodo */
+
         // Article::create([
-        //     'title' => 'A me non piace nessuno dei due',
-        //     'category' => 'Tempo libero' ,
+        //     'title' => 'Perché PHP è migliore di JS',
+        //     'category' => 'Programmazione PHP',
         //     'description' => '...',
         // ]);
 
+        // Article::create([
+        //     'title' => 'A me non piace nessuno dei due',
+        //     'category' => 'Tempo libero',
+        //     'description' => '...',
+        // ]);
     }
 }
+
+
+// ['title' => 'Perché JS è migliore di PHP', 'category' => 'Categoria: Programmazione JS', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', "visible" => true],
+// ['title' => 'Perché PHP è migliore di JS', 'category' => 'Categoria: Programmazione PHP', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', "visible" => true],
+// ['title' => 'A me non piace nessuno dei due', 'category' => 'Categoria: Programmazione generica', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', "visible" => true],
