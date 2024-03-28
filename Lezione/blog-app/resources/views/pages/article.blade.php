9205da375917780eb27c83392d43ebdc440a8e6e
Federico.Murru <djfedemurru@icloud.com>
2024-03-28 13:00:34 +0000

    .

--- Lezione/blog-app/resources/views/pages/article.blade.php
+++ Lezione/blog-app/resources/views/pages/article.blade.php
@@ -1,13 +1,18 @@
 <x-layout title="Article">
-        <div class="container">
+        {{-- <div class="container">
         <div class="mt-4">
             <h1 class="title">{{ $article['title'] }}</h1>
             <h4 class="title">{{ $article['category'] }}</h4>
             <p>{{ $article['description'] }}</p>
 
-     <a href="{{ route('articles') }}" class="btn btn-primary mb-3">Indietro</a>
 
         </div>
-    </div>
+    </div> --}}
+
+    <h1 class="title">{{ $article->title }}</h1>
+
+    <p>{{ $article->description }}</p>
+    
+    <a href="{{ route('articles') }}" class="btn btn-primary mb-3">Indietro</a>
 
 </x-layout>
