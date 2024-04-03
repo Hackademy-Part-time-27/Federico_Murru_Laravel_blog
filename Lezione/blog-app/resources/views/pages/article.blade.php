b6f740785cd1869256dcee70e82ae6fe0cf4682e
Federico.Murru <djfedemurru@icloud.com>
2024-04-03 18:38:38 +0100

    img

--- Lezione/blog-app/resources/views/pages/article.blade.php
+++ Lezione/blog-app/resources/views/pages/article.blade.php
@@ -12,6 +12,10 @@
     <h1 class="title">{{ $article->title }}</h1>
 
     <p>{{ $article->description }}</p>
+
+    @if($article->image)
+    <img class="img-fluid" src="{{ Storage::url($article->image) }}" alt="{{ $article->title }}">
+    @endif
     
     <a href="{{ route('articles') }}" class="btn btn-primary mb-3">Indietro</a>
 
